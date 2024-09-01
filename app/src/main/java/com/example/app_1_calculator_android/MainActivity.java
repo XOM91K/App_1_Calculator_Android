33a6a001e1cb929b17bc4ad12eceb8b57d8ff3b5
XOM91K <moohctp@gmail.com>
2024-09-01 11:43:30 +0300

    Добавляем обработчик события для кнопки.

--- app/src/main/java/com/example/app_1_calculator_android/MainActivity.java
+++ app/src/main/java/com/example/app_1_calculator_android/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     incorrectText.setVisibility(View.VISIBLE);
                     correctText.setVisibility(View.INVISIBLE);
+                    //
                 }
             }
         });
